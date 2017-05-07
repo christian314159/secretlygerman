85efea7df45d799114cb5cfc1fc425e20e5c963c
Christian Klammer <ck@hooroo.com>
2017-05-07 15:31:22 +1000

    Old site version

---
+++ js/dev/script.js
@@ -0,0 +1,348 @@
+'use strict';
+
+var PROJECTS = [
+	
+	{
+		"projectKey" : "aqui-site",
+		"name"       : "Aqui Promenade",
+		"type"       : "responsive website",
+		"images"     : [
+			"assets/work/aqui_site/aqui_site_2.jpg",
+			"assets/work/aqui_site/aqui_site_1.jpg",
+			"assets/work/aqui_site/aqui_site_3.jpg",
+			"assets/work/aqui_site/aqui_site_4.jpg",
+			"assets/work/aqui_site/aqui_site_5.jpg"
+		],
+		"description": "<h3>Modular template set for cross-device website</h3><p>This responsive website highlights the features and lifestyle of Aqui Promenade, a new development in Melbourne's Docklands.</p> <p>Built for the <span class='keyword'>Publisher CMS</span> whilst working at <span class='keyword'>BlocksGlobal</span>, all elements can easily be reordered and edited by the client. Though additional content comes up in lightboxes, it all exists under its own URL to improve <span class='keyword'>SEO</span>.</p> <p><span class='keyword'>GulpJS</span> is used to compile <span class='keyword'>SCSS</span> and minify JavaScript files. <span class='keyword'>Backbone</span> views help structuring the code.</p>",
+		"specs" : [
+			{
+				"name" : "URL",
+				"value" : "<a href='http://www.aquipromenade.com.au/' target='_blank'>Visit live site</a>"
+			},
+			{
+				"name" : "Date",
+				"value" : "2014"
+			},
+			{
+				"name" : "Agency",
+				"value" : "BlocksGlobal"
+			},
+			{
+				"name" : "CMS",
+				"value" : "Publisher"
+			},
+			{
+				"name" : "Client",
+				"value" : "MAB Corporation"
+			},
+			{
+				"name" : "Tags",
+				"value" : "SCSS, GulpJS, BackboneJS, jQuery, AJAX"
+			}
+		]
+	},
+	{
+		"projectKey" : "aqui-touch",
+		"name"       : "Aqui Promenade",
+		"type"       : "touch application",
+		"images"     : [
+			"assets/work/aqui_touch/aqui_touch_2.jpg",
+			"assets/work/aqui_touch/aqui_touch_1.jpg",
+			"assets/work/aqui_touch/aqui_touch_3.jpg",
+			"assets/work/aqui_touch/aqui_touch_5.jpg",
+			"assets/work/aqui_touch/aqui_touch_6.jpg",
+			"assets/work/aqui_touch/aqui_touch_7.jpg",
+			"assets/work/aqui_touch/aqui_touch_9.jpg"
+		],
+		"description": "<h3>Sales suite touchscreen application</h3><p>This single page touchscreen application was developed as a sales tool for the display suite of the Aqui Promenade development in Melbourne's Docklands. The app features several galleries, interactive maps as well as an apartment finder which allows a prospective buyer to select any level and apartment and view its floorplan and additional data.<br>It runs on two 42-inch 1080p touch screens in the display suite.</p> <p> Published to the touchscreens as a <span class='keyword'>Git</span> repository using BlocksGlobal's <span class='keyword'>digital signage service Screener</span>, this <span class='keyword'>Backbone Marionette</span> application draws all information from a local <span class='keyword'>JSON</span> file.</p><p class='project-note'>Please contact me for a code example of this project.</p>",
+		"specs" : [
+			{
+				"name" : "Date",
+				"value" : "2014"
+			},
+			{
+				"name" : "Agency",
+				"value" : "BlocksGlobal"
+			},
+			{
+				"name" : "Client",
+				"value" : "MAB Corporation"
+			},
+			{
+				"name" : "Tags",
+				"value" : "SCSS, GulpJS, BackboneJS, Marionette, jQuery, JSON, Touch, SVG"
+			}
+		]
+	},
+	{
+		"projectKey" : "lifeassist-site",
+		"name"       : "lifeAssist",
+		"type"       : "responsive website",
+		"images"     : [
+			"assets/work/life_assist/life_assist_2.jpg",
+			"assets/work/life_assist/life_assist_4.jpg",
+			"assets/work/life_assist/life_assist_1.jpg",
+			"assets/work/life_assist/life_assist_3.jpg"
+		],
+		"description": "<h3>Feature-rich responsive website</h3><p>Uniting Care lifeAssist assists vulnerable and disadvantaged people, as well as their carers and families. They offer a wide range of packages well catered for each specific situation.</p> <p>The site is fully responsive, consists of several flexible templates and was built for the <span class='keyword'>Publisher CMS</span>.<br> The site's key feature is a <a href='http://www.lifeassist.org.au/options-selector' target='_blank'>step-by-step mini-application</a> leading the user to the package offers for their individual needs. This <span class='keyword'>Backbone Marionette</span>/<span class='keyword'>RequireJS</span> app uses a <span class='keyword'>JSON</span> file holding all information on the relations between steps and packages.</p>",
+		"specs" : [
+			{
+				"name" : "URL",
+				"value" : "<a href='http://www.lifeassist.org.au/' target='_blank'>Visit live site</a>"
+			},
+			{
+				"name" : "Date",
+				"value" : "2014"
+			},
+			{
+				"name" : "Agency",
+				"value" : "BlocksGlobal"
+			},
+			{
+				"name" : "Client",
+				"value" : "Uniting Care Australia"
+			},
+			{
+				"name" : "CMS",
+				"value" : "Publisher"
+			},
+			{
+				"name" : "Tags",
+				"value" : "SCSS, GulpJS, BackboneJS, Marionette, jQuery, RequireJS, JSON"
+			}
+		]
+	},
+	{
+		"projectKey" : "kitchen-selector",
+		"name"       : "Kitchen Decor Selector",
+		"type"       : "touch application",
+		"images"     : [
+			"assets/work/kitchen_selector/01.jpg",
+			"assets/work/kitchen_selector/02.jpg",
+			"assets/work/kitchen_selector/03.jpg",
+			"assets/work/kitchen_selector/04.jpg"
+		],
+		"description": "<h3>Custom kitchen generator</h3><p>With hundreds of different decor options the possibilities are sheer endless. That is where this in-store touch application comes in and lets the customer create their very own kitchen by combining their favorite colours and previewing it directly. It also offers galleries and colour themes to inspire and flick through.</p><p>Having to play offline this <span class='keyword'>Backbone Marionette</span> app relies on a JSON file for configuration. A challenging task was displaying the selected colour swatches as textures on the kitchen model with its oddly shaped, not rectangular areas. <span class='keyword'>SVG</span> image masks solved this.</p><p class='project-note'>Please contact me for a code example of this project.</span>",
+		"specs" : [
+			{
+				"name" : "Date",
+				"value" : "2014"
+			},
+			{
+				"name" : "Agency",
+				"value" : "BlocksGlobal"
+			},
+			{
+				"name" : "Tags",
+				"value" : "jQuery, Backbone, Marionette, RequireJS, Touch, JSON"
+			}
+		]
+	}
+];
+
+var ProjectsController = (function(){
+
+	var projectTemplate,
+		projectCollection,
+		ProjectView,
+		current = {
+			view  : null,
+			model : null
+		},
+		loadedData = {},
+		$popup,
+		$projectTarget,
+		isPopupShowing = false;
+
+	// ------------------------------------------------------------- //
+
+	function init(){
+		projectTemplate   = getProjectTemplate();
+		projectCollection = new Backbone.Collection(PROJECTS);
+		ProjectView       = ProjectsController.ProjectView;
+
+		$popup            = $('.project-popup');
+		$projectTarget    = $('.project-details');
+
+		bindEvents();
+	}
+
+	function bindEvents(){
+		$('.project-link').click(function(e){
+			e.preventDefault();
+			showDetailsFor( $(this).data('project-key') );
+		});
+
+		$popup.on('click', function(e){
+			if( $(e.target).hasClass('project-popup') ){
+				hidePopup();
+			}
+		})
+		.on('click', '.backdrop, .close', function(e){
+			hidePopup();
+
+		}).on('click','.prev', function(e){
+			e.preventDefault();
+			showPrevProject();
+			return false;
+
+		}).on('click','.next', function(e){
+			e.preventDefault();
+			showNextProject();
+			return false;
+
+		});
+
+		$(document).on('keydown', function(e){
+			if( !isPopupShowing ){ return; }
+
+			var keyCode = e.which;
+
+			if( keyCode === 27 ){
+				hidePopup();
+			}
+			else if( keyCode === 37 ){
+				showPrevProject();
+			}
+			else if( keyCode === 39 ){
+				showNextProject();
+			}
+		});
+	}
+
+	function showPopup(){
+		isPopupShowing = true;
+		$('html,body').css('overflow','hidden');
+		$popup.fadeIn(400, function(){
+			$(document).trigger('popup-faded-in');
+		});
+	}
+
+	function hidePopup(){
+		isPopupShowing = false;
+		$('html,body').css('overflow','visible');
+		$popup.fadeOut(400, function(){
+			$(document).trigger('popup-faded-out');
+		});
+	}
+
+	function showDetailsFor( projectKey ){
+		var data  = getDataFor(projectKey);
+
+		if( current.view ){
+			current.view.unslick();
+		}
+
+		current = data;
+
+		// console.log('showDetailsFor', projectKey);
+		// console.log(data);
+
+		$projectTarget.empty().append( data.view.el );
+		setTimeout(function(){
+			data.view.initSlick();
+		}, isPopupShowing ? 50 : 500);
+		showPopup();		
+	}
+
+	function showPrevProject(){
+		var currentIndex = projectCollection.indexOf( current.model ),
+			newIndex    = currentIndex - 1 < 0 ? projectCollection.length - 1 : currentIndex - 1,
+			model        = projectCollection.at(newIndex);
+
+			// console.log('showPrevProject', currentIndex, newIndex, model);
+
+		showDetailsFor( model.get('projectKey') );
+	}
+
+	function showNextProject(){
+		var currentIndex = projectCollection.indexOf( current.model ),
+			newIndex    = currentIndex + 1 < projectCollection.length ? currentIndex + 1  : 0,
+			model        = projectCollection.at(newIndex);
+
+		// console.log('showNextProject', currentIndex, newIndex, model);
+
+		showDetailsFor( model.get('projectKey') );
+	}
+
+	// ------------------------------------------------------------- //
+
+	function getDataFor( projectKey ){
+		var data  = loadedData[projectKey] || {},
+			view  = data.view,
+			model = data.model;
+		
+		if( !view ){
+			model = projectCollection.findWhere({ projectKey : projectKey });
+			view  = new ProjectView({
+				model    : model,
+				template : projectTemplate
+			});
+			loadedData[projectKey] = {
+				view  : view,
+				model : model
+			};
+			view.render();
+		}
+
+		return {
+			view  : view,
+			model : model
+		};
+	}
+
+
+	function getProjectTemplate(){
+		return _.template( $('#project-template').html() );
+	}
+
+	// ------------------------------------------------------------- //
+
+	return {
+		init : init
+	}
+
+}());
+
+ProjectsController.ProjectView = Backbone.View.extend({
+
+	className : 'project-view',
+
+	events : {
+		'click .prev'  : 'prevProject',
+		'click .next'  : 'nextProject'
+	},
+
+	initialize : function(options){
+		this.template = options.template;
+	},
+
+	render : function(){
+		var json = this.model.toJSON(),
+			html = this.template(json);
+
+		this.$el.html( html );
+
+		return this;
+	},
+
+	initSlick : function(){
+		this.$('.gallery-inner').slick({
+			dots     : true,
+			arrows   : true,
+			lazyLoad : 'progressive'
+		});
+	},
+	unslick : function(){
+		this.$('.gallery-inner').unslick();
+	}
+});
+
+
+// DOM READY
+
+$(function(){
+
+	ProjectsController.init();
+
+});
